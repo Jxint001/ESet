8761389bc61bada9ab4dc87bb441a5c02d5bf14d
EmptyBox <925067182@qq.com>
2025-04-21 12:22:53 +0800

    copy and move

--- src.hpp
+++ src.hpp
@@ -11,15 +11,12 @@ public:
         return variant + " " + detail;
     }
 };
-class index_out_of_bound : public exception {};
-class runtime_error : public exception {};
 class invalid_iterator : public exception {};
-class container_is_empty : public exception {};
 
 template <class Key, class Compare = std::less<Key>>
 class ESet {
     struct Node{
-        Node *left, *right, *parent, *prev, *next;
+        Node *son[2], *parent, *prev, *next;
         enum color {BLACK, RED};
         Key key;
     };
@@ -31,17 +28,49 @@ class ESet {
 
     Node* subtree_max(const Node* u) {}
 
+    void clear() {
+        Node *cur = first, *nxt;
+        while (cur != nullptr) {
+            nxt = cur->next;
+            delete cur;
+            cur = nxt;
+        }
+        root = nullptr;  first = nullptr;
+        count = 0;
+    }
+
 public:    
     ESet();
-    ~ESet();
+    ~ESet() {clear(); }
 
     // 深拷贝
-    ESet( const ESet& other );
-    ESet& operator=( const ESet& other );
+    ESet(const ESet& other) {
+        for (auto it = other.begin(); it != other.end(); ++it) {
+            emplace(*it);
+        }
+    }
+    ESet& operator=(const ESet& other) {
+        if (this != &other) {
+            clear();
+            for (auto it = other.begin(); it != other.end(); ++it) {
+                emplace(*it);
+            }
+        }
+        return *this;
+    }
 
     // 移动。移动之后不应该有新增的空间，other应当被销毁。最高可接受复杂度 O(nlogn) 
-    ESet( ESet&& other );
-    ESet& operator=( ESet&& other ) noexcept;
+    ESet(ESet&& other) : root(other.root), first(other.first), count(other.count) {
+        other.root = nullptr;  other.first = nullptr;  other.count = 0;
+    }
+    ESet& operator=(ESet&& other) noexcept {
+        if (this != &other) {
+            clear();
+            root = other.root;  first = other.first;  count = other.count;
+            other.root = nullptr;  other.first = nullptr;  other.count = 0;
+        }
+        return *this;
+    }
 
     // 返回当前ESet总元素个数， O(1)
     size_t size() const noexcept {return count; }
@@ -93,19 +122,19 @@ public:
     iterator begin() const noexcept {return iterator(first, this); }
     iterator end() const noexcept {return iterator(nullptr, this); }
 
-    iterator find( const Key& key ) const;
+    iterator find(const Key& key) const;
+
+    // 返回[l,r]内元素的个数。若l>r，返回0。最高可接受复杂度 O(logn) 
+    size_t range(const Key& l, const Key& r) const;
+
+     // 返回最小的 >= key 的元素的迭代器/end()， O(logn) 
+    iterator lower_bound(const Key& key) const;
+
+    // 返回最小的 > key 的元素的迭代器/end()， O(logn) 
+    iterator upper_bound(const Key& key) const;
 
     template< class... Args >
     std::pair<iterator, bool> emplace( Args&&... args );
 
-    size_t erase( const Key& key );  // 有key返回1，无则返回0
-
-    // 返回[l,r]内元素的个数。若l>r，返回0。最高可接受复杂度 O(logn) 
-    size_t range( const Key& l, const Key& r ) const;
-
-    // 返回最小的 >= key 的元素的迭代器/end()， O(logn) 
-    iterator lower_bound( const Key& key ) const;
-
-    // 返回最小的 > key 的元素的迭代器/end()， O(logn) 
-    iterator upper_bound( const Key& key ) const;
+    size_t erase(const Key& key);  // 有key返回1，无则返回0
 };
