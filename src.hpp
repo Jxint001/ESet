009bb0ff950252640f1ddb49c9d3251b992d30fa
EmptyBox <925067182@qq.com>
2025-04-21 15:33:06 +0800

    emplace

--- src.hpp
+++ src.hpp
@@ -13,28 +13,31 @@ public:
     }
 };
 class invalid_iterator : public exception {};
-
+enum Color {BLACK, RED};
 template <class Key, class Compare = std::less<Key>>
 class ESet {
     struct Node{
         Node *son[2], *parent, *prev, *next;
-        enum color {BLACK, RED};
+        
+        Color color;
         Key key;
         size_t siz;
+        
+        Node(const Key& k) : key(k) {}
     };
     Node* root = nullptr;
     Node* first = nullptr;
     size_t count = 0;
 
     void UpdSiz(Node* u) {
-        u->siz = 1;
-        if (u->son[0]) {u->siz += u->son[0]->siz; }
-        if (u->son[1]) {u->siz += u->son[1]->siz; }
+        Node *cur = u;
+        while (cur != nullptr) {
+            cur->siz = 1;
+            if (cur->son[0]) {cur->siz += cur->son[0]->siz; }
+            if (cur->son[1]) {cur->siz += cur->son[1]->siz; }
+            cur = cur->parent;
+        }
     }
-
-    Node* subtree_min(const Node* u) {}
-
-    Node* subtree_max(const Node* u) {}
 
     void clear() {
         Node *cur = first, *nxt;
@@ -45,6 +48,12 @@ class ESet {
         }
         root = nullptr;  first = nullptr;
         count = 0;
+    }
+
+    // 以u为旋转中心的旋转，flg为0是左旋，否则右旋
+    // 注意旋转操作不影响prev和next，不影响color，影响son[2],parent和siz
+    void Rotate(Node* u, bool flg) {
+        
     }
 
 public:    
@@ -179,13 +188,117 @@ public:
     }
 
      // 返回最小的 >= key 的元素的迭代器/end()， O(logn) 
-    iterator lower_bound(const Key& key) const;
+    iterator lower_bound(const Key& key) const {
+        Node *cur = root, *ans = nullptr;
+        while (cur != nullptr) {
+            if (key < cur->key || key == cur->key) {
+                ans = cur;
+                cur = cur->son[0];
+            } else {
+                cur = cur->son[1];
+            }
+        }
+        return iterator(ans, this);
+    }
 
     // 返回最小的 > key 的元素的迭代器/end()， O(logn) 
-    iterator upper_bound(const Key& key) const;
+    iterator upper_bound(const Key& key) const {
+        Node *cur = root, *ans = nullptr;
+        while (cur != nullptr) {
+            if (key < cur->key) {
+                ans = cur;
+                cur = cur->son[0];
+            } else {
+                cur = cur->son[1];
+            }
+        }
+        return iterator(ans, this);
+    }
 
     template< class... Args >
-    std::pair<iterator, bool> emplace( Args&&... args );
+    std::pair<iterator, bool> emplace( Args&&... args ) {
+        Key key(std::forward<Args>(args)...);
+        if (!count) {
+            root = new Node(key);
+            root->color = BLACK;  root->siz = 1;
+            first = root;
+            ++count;
+            return {begin(), 1};
+        }
+        // 获取插入的位置(pos是插入节点的父节点)
+        Node* pos = root;
+        while (true) {
+            if (key < pos->key) {
+                if (pos->son[0] == nullptr) break;
+                else  pos = pos->son[0];
+            } else if (pos->key < key) {
+                if (pos->son[1] == nullptr) break;
+                else  pos = pos->son[1];
+            } else {
+                return {iterator(pos), 0};
+            }
+        }
+        ++count;
+        Node *new_node = new Node(key);  new_node->color = RED;  new_node->siz = 1;
+        // 对双红的处理不影响双向链表（中序遍历），可以在前面就处理好prev和next
+        if (key < pos->key) {
+            pos->son[0] = new_node;
+            Node *pre = pos->prev;
+            new_node->next = pos;
+            new_node->prev = pre;
+            pos->prev = new_node;
+            if (pre != nullptr) {pre->next = new_node; }
+            else {first = new_node; }
+        } else {
+            pos->son[1] = new_node;
+            Node *nxt = pos->next;
+            new_node->prev = pos;
+            new_node->next = nxt;
+            pos->next = new_node;
+            if (nxt != nullptr) {nxt->prev = new_node; }
+        }
+        UpdSiz(pos);
+        if (pos->color == BLACK)  return {iterator(new_node), 1};      
+
+        // 处理双红的情况
+        // 获取叔叔节点，pos->parent必然存在，因为pos是红的，意味着pos不是根
+        Node* cur = new_node;
+        while (cur != root && cur->parent->color == RED) {
+            Node* father = cur->parent;
+            Node* grand = cur->parent->parent;
+            Node *uncle = grand->son[father != grand->son[1]];
+            if (uncle == nullptr || uncle->color = BLACK) {
+                if (father == grand->son[0]) {  // L
+                    if (cur == father->son[0]) {  // LL
+                        Rotate(grand, 1);
+                        father->color = BLACK;  grand->color = RED;
+                    } else {  // LR
+                        Rotate(father, 0);
+                        Rotate(grand, 1);
+                        cur->color = BLACK;  grand->color = RED;
+                    }
+                } else { // R
+                    if (cur == father->son[1]) {  // RR
+                        Rotate(grand, 0);
+                        father->color = BLACK;  grand->color = RED;
+                    } else { // RL
+                        Rotate(father, 1);
+                        Rotate(grand, 0);
+                        cur->color = BLACK;  grand->color = RED;
+                    }
+                }
+            }
+            if (uncle->color == RED) {
+                // 变色
+                father->color = uncle->color = BLACK;
+                grand->color = RED;
+                // 假装插入爷爷，递归处理
+                cur = grand;
+                if (cur == root)  cur->color = BLACK;
+            }
+        }
+        return {iterator(new_node), 1};
+    }
 
     size_t erase(const Key& key);  // 有key返回1，无则返回0
 };
